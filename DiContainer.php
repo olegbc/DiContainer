0a96a9d459c097e1f460443cb7330446f5602caa
olegkamuz <olegkamuz@gmail.com>
2016-08-09 18:30:01 +0300

    commit three

--- DiContainer.php
+++ DiContainer.php
@@ -1,46 +1,107 @@
 <?php
 
 include ('RelatedClassInterface.php');
-include ('UserProfile.php');
+include ('RelatedClassOne.php');
+include ('Container.php');
 include ('User.php');
 
-class DiContainer
+class DiContainer implements Container
 {
-    private $relatedClassParams = [];
-    private $relatedClassName = '';
-    private $mainClassName = '';
-
-    public function __construct($mainClassName, $relatedClassName,  array $relatedClassParams = [])
+    public static function getInstance()
     {
-        $this->relatedClassParams = $relatedClassParams;
-        $this->relatedClassName = $relatedClassName;
-        $this->mainClassName = $mainClassName;
+        return new static();
+    }
+    private function __construct()
+    {
     }
 
-    private function getRelatedClass()
+    private static $class;
+    //private  $relatedClassName = 'RelatedClassOne';
+
+    public function get($class)
     {
-        $class = new $this->relatedClassName;
-        foreach ($this->relatedClassParams as $key=>$val) {
-            $class->$key = $val;
+        if(!is_null(self::$class)) {
+            return $this->class;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $class
+     * @param array $arguments
+     * @return mixed
+     */
+    public function create($class, array $arguments = [])
+    {
+        if(is_null(self::$class)) {
+            $mainClassObject = new $class(new GuestUser(), $arguments);
+            $this->class = $mainClassObject;
+            //$mainClassObject->setRelatedClassDependencies($this->getRelatedClass());
+            return $mainClassObject;
         }
 
-        return $class;
-    }
-
-    public function getMainClass()
-    {
-        $mainClassObject = new $this->mainClassName;
-        $mainClassObject->setRelatedClassDependency($this->getRelatedClass());
-
-        return $mainClassObject;
+        return self::$class;
     }
 }
 
-$diContainer = new DiContainer('User', 'UserProfile', ['firstName' => 'John', 'lastName' => 'Smith', 'email' => 'smith@gmail.com']);
 
-$user = $diContainer->getMainClass();
+class GuestUser {
+    private $name;
+    /**
+     * @var Bob
+     */
+    private $bob;
 
-var_dump($user);
+    public function __construct(Bob $bob, $name = 'guest')
+    {
+        $this->name = $name;
+        $this->bob = $bob;
+    }
+
+}
+
+class TestUser {
+
+    private $name;
+    /**
+     * @var GuestUser
+     */
+    private $sub;
+    private $test;
+
+    public function __construct(GuestUser $sub, $name, $test)
+    {
+        $this->name = $name;
+        $this->sub = $sub;
+        $this->test = $test;
+    }
+
+}
+
+class Bob {
+    /**
+     * @var Jon
+     */
+    private $jon;
+
+    public function __construct(Jon $jon)
+    {
+        $this->jon = $jon;
+    }
+
+}
+
+class Jon {
+
+}
+
+$diContainer = DiContainer::getInstance();
+
+$user = $diContainer->create(TestUser::class, ['test' => 123, 'name' => 'Bob']);
+$user2 = $diContainer->create(TestUser::class, ['test' => 888, 'name' => 'Jon']);
+
+var_dump($user); // Bob
+var_dump($user2); // Jon
 
 
 
